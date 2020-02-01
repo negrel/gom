268f2f561f019947c71e6b20598b6c3fe6a236cd
negrel <alexnergel13@gmail.com>
2020-02-01 14:29:44 +0100

    improving test log message for all test

--- node_test.go
+++ node_test.go
@@ -12,16 +12,18 @@ func TestAppendChild(t *testing.T) {
 	// Appending the child
 	child = node.AppendChild(child)
 
-	// If node doesn't contain child, test failed
+	// Check that node contains the child
 	if contain := node.Contains(child); !contain {
 		t.Log("Node must contain the appended child.")
 		t.Logf("Node number of child : %v", node.ChildNodes().Length())
 		t.Fail()
 	}
 
-	// If child parent node is not the same as node, test failed
+	// Check that child parent is node
 	if isParent := child.ParentNode().IsSameNode(node); !isParent {
-		t.Log("Node must be the parent of the appended child.")
+		t.Log("Node must be the parent of the appended child. (pointer must be the same)")
+		t.Logf("Child parent node pointer address : %p", child.ParentNode())
+		t.Logf("Node pointer address              : %p", node)
 		t.Fail()
 	}
 }
@@ -47,7 +49,7 @@ func TestCloneNode(t *testing.T) {
 
 	clone = node.CloneNode(true)
 
-	// Clone must be equal (deep clone)
+	// Checking that clone is equal to node
 	if equal := clone.IsEqualNode(node); !equal {
 		t.Log("Clone must be equal to node. (deep clone)")
 		t.Logf("Clone child node : %v", clone.ChildNodes())
@@ -55,7 +57,7 @@ func TestCloneNode(t *testing.T) {
 		t.Fail()
 	}
 
-	// Clone must not be the same
+	// Checking that clone doesn't point to node
 	if same := clone.IsSameNode(node); same {
 		t.Log("Clone must not point to the same reference than node.")
 		t.Logf("Clone pointers address : %p", clone)
@@ -83,7 +85,9 @@ func TestContains(t *testing.T) {
 
 	// Checking that node doesn't contain child2
 	if contain := node.Contains(child2); contain {
-		t.Log("Node must not contain child2.")
+		t.Log("Node must not contain child2. (pointer must be different)")
+		t.Logf("Node childrens pointer address : %v", node.ChildNodes().Values())
+		t.Logf("Child2 pointer address         : %p", child2)
 		t.Fail()
 	}
 
@@ -91,13 +95,17 @@ func TestContains(t *testing.T) {
 
 	// Checking that child1 contain child2
 	if contain := child1.Contains(child2); !contain {
-		t.Log("Child1 must contain child2. (direct child)")
+		t.Log("Child1 must contain child2 (direct child). (pointer must be the same)")
+		t.Logf("Child1 childrens pointer address : %v", child1.ChildNodes().Values())
+		t.Logf("Child2 pointer address           : %p", child2)
 		t.Fail()
 	}
 
 	// Checking that node contain child2 (child of child1)
 	if contain := node.Contains(child2); !contain {
-		t.Log("Node must contain child2. (direct child of child1)")
+		t.Log("Node must contain child2 (direct child of child1). (pointer must be the same)")
+		t.Logf("Child1 childrens pointer address : %v", child1.ChildNodes().Values())
+		t.Logf("Child2 pointer address           : %p", child2)
 		t.Fail()
 	}
 }
@@ -111,7 +119,7 @@ func TestGetRootNode(t *testing.T) {
 
 	// Checking that node root is node
 	if same := node.GetRootNode().IsSameNode(node); !same {
-		t.Log("Node root node must be node itself")
+		t.Log("Node root node must be node itself. (pointer must be the same)")
 		t.Logf("Node root pointer address : %p", node.GetRootNode())
 		t.Logf("Node pointer address      : %p", node)
 		t.Fail()
@@ -119,7 +127,7 @@ func TestGetRootNode(t *testing.T) {
 
 	// Checking that child root is node
 	if same := child1.GetRootNode().IsSameNode(node); !same {
-		t.Log("Node must be the root node of child1")
+		t.Log("Node must be the root node of child1. (pointer must be the same)")
 		t.Logf("Child1 root pointer address : %p", child1.GetRootNode())
 		t.Logf("Node pointer address        : %p", node)
 		t.Fail()
@@ -127,7 +135,7 @@ func TestGetRootNode(t *testing.T) {
 
 	// Checking that child2 root is not node
 	if same := child2.GetRootNode().IsSameNode(node); same {
-		t.Log("Node must not be the root node of child2")
+		t.Log("Node must not be the root node of child2. (pointer must not be the same)")
 		t.Logf("Child2 root pointer address : %p", child2.GetRootNode())
 		t.Logf("Node pointer address        : %p", node)
 		t.Fail()
