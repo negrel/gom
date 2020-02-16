dcaf11c85ec553aef02891b732e915dd677d0e9f
negrel <negrel.dev@protonmail.com>
2020-02-16 17:19:18 +0100

    completing attr interface

--- attr.go
+++ attr.go
@@ -5,4 +5,29 @@ package gom
 // https://developer.mozilla.org/en-US/docs/Web/API/Attr
 // https://dom.spec.whatwg.org/#attr
 type Attr interface {
+	Name() string
+	OwnerElement() Element
+	Value() string
+}
+
+type attr struct {
+	name         string
+	ownerElement Element
+	value        string
+}
+
+// Name return the attribute name of an element
+// https://developer.mozilla.org/en-US/docs/Web/API/Attr/localName
+func (a *attr) Name() string {
+	return a.name
+}
+
+// OwnerElement return the element holding the attribute
+func (a *attr) OwnerElement() Element {
+	return a.ownerElement
+}
+
+// Value return the attribute value of an element
+func (a *attr) Value() string {
+	return a.value
 }
