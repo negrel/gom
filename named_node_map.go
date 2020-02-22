11dbc563d77ac44eeba44608f4e3e471e5d41b87
negrel <negrel.dev@protonmail.com>
2020-02-22 22:19:51 +0100

    adding item method for named node map

--- named_node_map.go
+++ named_node_map.go
@@ -22,18 +22,11 @@ var _ NamedNodeMap = &namedNodeMap{}
 
 type namedNodeMap struct {
 	dict map[string]Attr
-	arr  []Attr
 }
 
 func (n *namedNodeMap) getNamedItem(name string) (Attr, bool) {
 	attr, ok := n.dict[name]
 	return attr, ok
-}
-
-func (n *namedNodeMap) sortArray() {
-	sort.Slice(n.arr, func(i, j int) bool {
-		return n.arr[i].Name() < n.arr[j].Name()
-	})
 }
 
 /*****************************************************
@@ -56,8 +49,20 @@ func (n *namedNodeMap) GetNamedItem(name string) Attr {
 
 // Item returns the Attr at the given index, or null if
 // the index is higher or equal to the number of nodes.
+// (Slower than GetNamedItem)
 func (n *namedNodeMap) Item(index int) Attr {
-	return n.arr[index]
+	arr := make([]Attr, n.Length())
+
+	for _, value := range n.dict {
+		arr = append(arr, value)
+	}
+
+	// Alphabetical sort
+	sort.Slice(arr, func(i, j int) bool {
+		return arr[i].Name() < arr[j].Name()
+	})
+
+	return arr[index]
 }
 
 // SetNamedItem Replaces, or adds, the Attr identified
@@ -65,8 +70,6 @@ func (n *namedNodeMap) Item(index int) Attr {
 func (n *namedNodeMap) SetNamedItem(attr Attr) {
 	// Set the new attribute value
 	n.dict[attr.Name()] = attr
-
-	n.sortArray()
 }
 
 // RemoveNamedItem remove the specified attribute.
