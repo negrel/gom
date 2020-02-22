64cc4d2d359918a867e738b99b298cdd5d372d14
negrel <negrel.dev@protonmail.com>
2020-02-22 19:56:38 +0100

    adding item method for named node map

--- named_node_map.go
+++ named_node_map.go
@@ -1,5 +1,7 @@
 package gom
 
+import "sort"
+
 // NamedNodeMap interface represents a collection
 // of Attr objects.
 // https://developer.mozilla.org/en-US/docs/Web/API/NamedNodeMap
@@ -19,13 +21,19 @@ type NamedNodeMap interface {
 var _ NamedNodeMap = &namedNodeMap{}
 
 type namedNodeMap struct {
-	dict map[string]*attr
-	arr  NodeList
+	dict map[string]Attr
+	arr  []Attr
 }
 
 func (n *namedNodeMap) getNamedItem(name string) (Attr, bool) {
 	attr, ok := n.dict[name]
 	return attr, ok
+}
+
+func (n *namedNodeMap) sortArray() {
+	sort.Slice(n.arr, func(i, j int) bool {
+		return n.arr[i].Name() < n.arr[j].Name()
+	})
 }
 
 /*****************************************************
@@ -49,7 +57,7 @@ func (n *namedNodeMap) GetNamedItem(name string) Attr {
 // Item returns the Attr at the given index, or null if
 // the index is higher or equal to the number of nodes.
 func (n *namedNodeMap) Item(index int) Attr {
-	return n.arr.Item(index)
+	return n.arr[index]
 }
 
 // SetNamedItem Replaces, or adds, the Attr identified
@@ -57,19 +65,20 @@ func (n *namedNodeMap) Item(index int) Attr {
 func (n *namedNodeMap) SetNamedItem(attr Attr) {
 	// Set the new attribute value
 	n.dict[attr.Name()] = attr
+
+	n.sortArray()
 }
 
 // RemoveNamedItem remove the specified attribute.
 func (n *namedNodeMap) RemoveNamedItem(name string) (Attr, GOMError) {
-	ptrAttr := n.dict[name]
-	attr := *ptrAttr
+	attr := n.dict[name]
 
 	// Check if attribute exist
-	if ptrAttr == nil {
+	if attr == nil {
 		return nil, newGOMError("The attr to be removed is not part of this element", "ErrNotFound")
 	}
 
 	delete(n.dict, name)
 
-	return &attr, nil
+	return attr, nil
 }
